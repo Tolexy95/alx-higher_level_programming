26cd4cdecf0774d5052cc60177d885f11648d906
Tolexy95 <toyin95.olubayo@gmail.com>
2022-05-31 14:51:18 -0700

    Task 6 redo

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-for i in range(0, 9):
-    for j in range(1, 10):
-        if j > i:
-            if i != 8:
-                print("{}{}, ".format(i, j), end="")
-            else:
-                print("{}{}".format(i, j))
+for digit1 in range(0, 9):
+    for digit2 in range(digit1 + 1, 10):
+        if digit1 == 8:
+            print("{:d}{:d}".format(digit1, digit2))
+            break
+        print("{:d}{:d}".format(digit1, digit2), end=", ")
